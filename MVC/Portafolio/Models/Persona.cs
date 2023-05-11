25ebd8df068241aa7318d67d9388a81a1a38fba1
Alex <alexxxd23@gmail.com>
2023-05-10 22:22:32 -0300

    added changes on proyect

---
+++ MVC/Portafolio/Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿namespace Portafolio.Models
+{
+    public class Persona
+    {
+        public String? Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public Persona()
+        { }
+        public Persona(String nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+    }
+}
