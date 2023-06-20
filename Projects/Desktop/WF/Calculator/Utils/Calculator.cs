7dca28c1bfc7fd60e7dbe8162d79a49229304678
alex <alexxxd23@gmail.com>
2023-06-19 23:10:49 -0300

    create class calculator

---
+++ Projects/Desktop/WF/Calculator/Utils/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Utils
+{
+    public static class Calculator
+    {
+        static double Sum(double number, double number2) => number + number2;
+        static double Substract(double number, double number2) => number - number2;
+        static double Multiplication(double number, double number2) => number * number2;
+        static double Division(double number, double number2) => number / number2;
+    }
+}
